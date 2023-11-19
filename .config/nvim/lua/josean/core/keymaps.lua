371b5d31ed4363a44d487b8c53210795613ef593
Abdul Rosyid <ocritocrit@gmail.com>
2023-11-20 06:45:59 +0700

    feat: add nvim with lazy

---
+++ .config/nvim/lua/josean/core/keymaps.lua
@@ -0,0 +1,51 @@
+-- set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps -------------------
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- for save on normal & insert mode
+keymap.set("n", "<C-s>", "<cmd>write<CR>", { desc = "Save file" })
+
+--open lazy
+keymap.set("n", "<leader>ll", ":Lazy<CR>", { desc = "Lazy" })
+
+--open Mason
+keymap.set("n", "<leader>cm", ":Mason<CR>", { desc = "Lazy" })
+
+--open lazygit
+keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "open Lazygit" })
+
+-- quit nvim
+keymap.set("n", "<leader>q", ":quitall<CR>", { desc = "Quit NVIM" })
+
+-- set wrap or nowrap
+keymap.set("n", "<leader>uu", ":set wrap<CR>", { desc = "set line wrap" })
+keymap.set("n", "<leader>uw", ":set nowrap<CR>", { desc = "set line nowrap" })
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- delete single character without copying into register
+-- keymap.set("n", "x", '"_x')
+
+--  increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
